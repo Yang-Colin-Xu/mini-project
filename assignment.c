76db9b8900fdc1211c4cc8924d734e830f21e33d
Yang-Colin-Xu <yx81@kent.ac.uk>
2024-11-04 15:59:54 +0000

    Update assignment.c

--- assignment.c
+++ assignment.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-#define NUM_SUBJECT 3
-#define NUM_STUDENT 2
+#define NUM_SUBJECTS 3
+#define NUM_STUDENTS 2
 
 int main(){
   
-  
+  char names[NUM_STUDENTS][50];
+  int scores[NUM_STUDENTS][NUM_SUBJECTS];
+  float averages[NUM_STUDENTS];
+  char grades[NUM_STUDENTS];
