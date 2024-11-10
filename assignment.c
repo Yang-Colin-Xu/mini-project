3898ff7d74b68af93198507332d0247989a5c3e8
Yang-Colin-Xu <yx81@kent.ac.uk>
2024-11-11 00:50:21 +0800

    Update assignment.c

--- assignment.c
+++ assignment.c
@@ -3,12 +3,12 @@
 #define NUM_STUDENTS 2
 
 int main(){
-  
+  //declare variables
   char names[NUM_STUDENTS][50];
   int scores[NUM_STUDENTS][NUM_SUBJECTS];
   float averages[NUM_STUDENTS];
   char grades[NUM_STUDENTS];
-
+  //Input stdent information
   for(int i = ; i < NUM_STUDENTS; i++) {
     printf("Enter name of student%d:", i + 1);
     scanf("%s", names[i]);
@@ -16,14 +16,15 @@ int main(){
     printf("Enter scores for %s in %d subject;\n", names[i], NUM_SUBJECTS);
     int total = 0;
 
-
+    //Input scores and calculate total
     for (int j = 0; j < NUM_SUBJECTS; j++) {
         printf("Score for subject %d: ", j + 1);
         scanf("%d" , &scores[i][j]);
     }
-
+    //calculate average score
+  
     averages[i] = (float)total / NUM_SUBJECTS;
-
+  //Determine grade
     switch ((int)averages[i] / 10) {
       case 10:
       case 9: grades[i] = 'A'; break;
@@ -33,7 +34,13 @@ int main(){
       default: grades[i] = 'F'; break;
     }
   }
-
+  //Results
+  printf("\nStudent Grade:\n");
+  for(int i = 0; i < NUM_STUDENTS; i++) {
+    printf("Name: %s\n", name[i]);
+    printf(" Average Score: %.2f\n", averages[i]);
+    printf("Grade: %c\n\n", grades[i]);
+    
   return 0; 
 }
 
